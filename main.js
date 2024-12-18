5ae60ca636b42d3cd4566207489766ae6e526740
Aonnoy Sengupta <70891467+aonnoy@users.noreply.github.com>
2024-12-18 16:56:14 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
-import { loadStylesheet, loadScript } from "./external-script-loader.js";
 
-// Load Swiper CSS
-loadStylesheet(
-  "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css",
-  () => {
-    console.log("Swiper CSS loaded!");
-  }
-);
+// Check the origin and decide which script to load
+const origin = window.location.origin;
 
-// Load Swiper JS
-loadScript(
-  "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js",
-  () => {
-    console.log("Swiper JS loaded!");
-    // Initialize Swiper here
-  }
-);
+if (origin.includes("webflow.io")) {
+  // Load the staging script
+  loadDynamicScript("home/staging.js", () => {
+    console.log("Loaded staging script");
+  });
+} else {
+  // Load the production script
+  loadDynamicScript("home/production.js", () => {
+    console.log("Loaded production script");
+  });
+}
