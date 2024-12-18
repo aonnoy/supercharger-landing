cbcfab45b3f39cf794b0cbd8b41427baeef48a92
Aonnoy Sengupta <70891467+aonnoy@users.noreply.github.com>
2024-12-18 17:03:20 +0000

    Update main.js

--- home/main.js
+++ home/main.js
@@ -13,12 +13,12 @@ const origin = window.location.origin;
 
 if (origin.includes("webflow.io")) {
   // Load the staging script
-  loadDynamicScript("home/staging.js", () => {
-    console.log("Loaded staging script");
-  });
+loadDynamicScript("/home/staging.js", () => {
+  console.log("Loaded staging script");
+});
 } else {
   // Load the production script
-  loadDynamicScript("home/production.js", () => {
-    console.log("Loaded production script");
-  });
+loadDynamicScript("/home/production.js", () => {
+  console.log("Loaded production script");
+});
 }
