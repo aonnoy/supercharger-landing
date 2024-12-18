4bb9bc6a9d1fb2b1804c615a773678f56bddf819
Aonnoy Sengupta <70891467+aonnoy@users.noreply.github.com>
2024-12-18 16:53:57 +0000

    Update staging.js

--- home/staging.js
+++ home/staging.js
@@ -1,1 +1,18 @@
+import { loadStylesheet, loadScript } from "./external-script-loader.js";
 
+// Load Swiper CSS
+loadStylesheet(
+  "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css",
+  () => {
+    console.log("Swiper CSS loaded!");
+  }
+);
+
+// Load Swiper JS
+loadScript(
+  "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js",
+  () => {
+    console.log("Swiper JS loaded!");
+    // Initialize Swiper here
+  }
+);
