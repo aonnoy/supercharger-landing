f27986e8e3abd0f7406df063e61446830a5d52a5
Aonnoy Sengupta <70891467+aonnoy@users.noreply.github.com>
2024-12-18 16:04:57 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+import { loadStylesheet, loadScript } from "./swiper-js-loader.js";
 
+document.addEventListener("DOMContentLoaded", () => {
+  // Load Swiper CSS
+  loadStylesheet(
+    "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css",
+    () => {
+      console.log("Swiper CSS loaded!");
+    }
+  );
+
+  // Load Swiper JS
+  loadScript(
+    "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js",
+    () => {
+      console.log("Swiper JS loaded!");
+      // Initialize Swiper here
+    }
+  );
+});
