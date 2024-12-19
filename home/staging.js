1aec6065811c1977534f2226e8a230d25d8a2f00
Aonnoy Sengupta <70891467+aonnoy@users.noreply.github.com>
2024-12-19 12:23:41 +0000

    Update staging.js

--- home/staging.js
+++ home/staging.js
@@ -1,3 +1,3 @@
+// INITIALISE SWIPER JS ON THE ORDER FORM
 import 'https://supercharger-staging.vercel.app/home/scripts/staging/swiper-order-form.js';
-
 console.log('Swiper order form script loaded!');
