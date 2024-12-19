a0b3fc61368189338ac0ca4914a6839a95922497
Aonnoy Sengupta <70891467+aonnoy@users.noreply.github.com>
2024-12-19 21:04:40 +0000

    Update staging.js

--- home/staging.js
+++ home/staging.js
@@ -9,13 +9,16 @@ import 'https://supercharger-staging.vercel.app/home/scripts/staging/product-sel
 console.log('Product selection hover and clicked Function is loaded');
 
 
-// THIS SCRIPT ENSURES THAT WHEN HOVERING/CLICKING OVER PRODUCT SELECTION RADIO'S THE "LABEL" INSIDE IT
-// HAS THE COMBO CLASS OF "is-active" ADDED TO IT
+
 import 'https://supercharger-staging.vercel.app/home/scripts/staging/search-add-patent.js';
 console.log('Search add patent function is loaded');
 
 
-// THIS SCRIPT ENSURES THAT WHEN HOVERING/CLICKING OVER PRODUCT SELECTION RADIO'S THE "LABEL" INSIDE IT
-// HAS THE COMBO CLASS OF "is-active" ADDED TO IT
+
 import 'https://supercharger-staging.vercel.app/home/scripts/staging/prior-art-preview-set-variable.js';
+console.log('Set Prior art Preview variable function is loaded');
+
+
+
+import 'https://supercharger-staging.vercel.app/home/scripts/staging/prior-art-truncation.js';
 console.log('Set Prior art Preview variable function is loaded');
