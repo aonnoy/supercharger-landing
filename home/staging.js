1d5f0ab920aef274e858271ba0671f1aff7dec98
Aonnoy Sengupta <70891467+aonnoy@users.noreply.github.com>
2024-12-19 19:01:55 +0000

    Update staging.js

--- home/staging.js
+++ home/staging.js
@@ -2,6 +2,8 @@
 import 'https://supercharger-staging.vercel.app/home/scripts/staging/swiper-order-form.js';
 console.log('Swiper order form script loaded!');
 
-// INITIALISE SWIPER JS ON THE ORDER FORM
+
+// THIS SCRIPT ENSURES THAT WHEN HOVERING/CLICKING OVER PRODUCT SELECTION RADIO'S THE "LABEL" INSIDE IT
+// HAS THE COMBO CLASS OF "is-active" ADDED TO IT
 import 'https://supercharger-staging.vercel.app/home/scripts/staging/product-selection-radio-hover.js';
 console.log('Product selection hover and clicked Function is loaded');
