f1dd448a2681715fbada1e29d8c964c78f932bc8
Aonnoy Sengupta <70891467+aonnoy@users.noreply.github.com>
2024-12-19 14:53:29 +0000

    Update custom-css.js

--- utilities/custom-css.js
+++ utilities/custom-css.js
@@ -1,1 +1,6 @@
-
+// Function to inject custom CSS
+function addCustomStyles(css) {
+  const style = document.createElement('style');
+  style.textContent = css;
+  document.head.appendChild(style);
+}
