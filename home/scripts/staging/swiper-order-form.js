321655d6c5ad6aed90bf180a82b8d79d16c04d0e
Aonnoy Sengupta <70891467+aonnoy@users.noreply.github.com>
2024-12-19 10:03:03 +0000

    Update swiper-order-form.js

--- home/scripts/staging/swiper-order-form.js
+++ home/scripts/staging/swiper-order-form.js
@@ -1,1 +1,19 @@
 
+import { loadStylesheet, loadScript } from "https://supercharger-staging.vercel.app/utilities/external-script-loader.js";
+
+// Load Swiper CSS
+loadStylesheet(
+  "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css",
+  () => {
+    console.log("Swiper CSS loaded!");
+  }
+);
+
+// Load Swiper JS
+loadScript(
+  "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js",
+  () => {
+    console.log("Swiper JS loaded!");
+    // Initialize Swiper here if needed
+  }
+);
