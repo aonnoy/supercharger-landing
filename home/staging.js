bcbc5f565702cf12662bdefcd1491c5e46d36063
Aonnoy Sengupta <70891467+aonnoy@users.noreply.github.com>
2024-12-19 18:53:51 +0000

    Update staging.js

--- home/staging.js
+++ home/staging.js
@@ -4,4 +4,4 @@ console.log('Swiper order form script loaded!');
 
 // INITIALISE SWIPER JS ON THE ORDER FORM
 import 'https://supercharger-staging.vercel.app/home/scripts/staging/product-selection-radio-hover.js';
-console.log('Product Selection Hover Function is loaded');
+console.log('Product selection hover and clicked Function is loaded');
