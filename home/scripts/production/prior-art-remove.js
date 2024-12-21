f4d734e02e2c83fcf6648d74e18ac07a31ec6834
Aonnoy Sengupta <70891467+aonnoy@users.noreply.github.com>
2024-12-21 13:54:33 +0000

    Update prior-art-remove.js

--- home/scripts/production/prior-art-remove.js
+++ home/scripts/production/prior-art-remove.js
@@ -1,4 +1,4 @@
-import { initializeTruncationListeners } from 'https://supercharger-staging.vercel.app/home/scripts/staging/prior-art-truncation.js';
+import { initializeTruncationListeners } from 'https://supercharger-staging.vercel.app/home/scripts/production/prior-art-truncation.js';
 
 window.Wized = window.Wized || [];
 window.Wized.push((Wized) => {
